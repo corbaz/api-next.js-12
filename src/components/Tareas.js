47b24ab9271daddab9cf6bf7723322e52ddaab6b
Julio Corbaz <julio.corbaz@gmail.com>
2021-12-12 14:22:26 -0300

    'update'

--- src/components/Tareas.js
+++ src/components/Tareas.js
@@ -5,28 +5,73 @@ export default function Tareas() {
 
   return (
     <>
-      <h3 className="text-3xl text-center my-5">Lista de Tareas</h3>
-      <div className="overflow-x-auto">
-        <table className="table w-full">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Tarea</th>
-              <th>Descripción</th>
-              <th>Estados</th>
-            </tr>
-          </thead>
-          <tbody>
+      <div
+        className="bg-white shadow-md rounded-lg border
+                      max-w-md p-4 sm:p-8 dark:bg-gray-800
+                      dark:border-gray-700 mx-auto"
+      >
+        <div className="flex items-center justify-center mb-4">
+          <h2
+            className="text-xl font-bold leading-none text-indigo-600
+                       dark:text-white"
+          >
+            Listado de Tareas
+          </h2>
+        </div>
+        <div className="flow-root">
+          <ul
+            role="list"
+            className="divide-y divide-gray-200 dark:divide-gray-700"
+          >
             {tasks.map((task) => (
-              <tr key={task.id} className="">
-                <th>{task.id}</th>
-                <td>{task.title}</td>
-                <td>{task.description}</td>
-                <td>{task.status}</td>
-              </tr>
+              <li key={task.id} className="py-3 sm:py-4">
+                <div className="flex items-center space-x-4">
+                  <div className="flex-1 min-w-0">
+                    <p
+                      className={`text-sm font-medium truncate dark:text-white 
+                        ${
+                          task.status === "Pendiente"
+                            ? "text-red-600"
+                            : task.status == "Activo"
+                            ? "text-green-600"
+                            : "text-blue-600"
+                        }
+                      `}
+                    >
+                      {task.title}
+                    </p>
+                    <p
+                      className={`text-sm truncate dark:text-gray-400 
+                         ${
+                           task.status == "Pendiente"
+                             ? "text-red-600"
+                             : task.status == "Activo"
+                             ? "text-green-600"
+                             : "text-blue-600"
+                         }
+                      `}
+                    >
+                      {task.description}
+                    </p>
+                  </div>
+                  <div
+                    className={`inline-flex items-center text-base font-semibold dark:text-white 
+                      ${
+                        task.status == "Pendiente"
+                          ? "text-red-600"
+                          : task.status == "Activo"
+                          ? "text-green-600"
+                          : "text-blue-600"
+                      }
+                    `}
+                  >
+                    {task.status}
+                  </div>
+                </div>
+              </li>
             ))}
-          </tbody>
-        </table>
+          </ul>
+        </div>
       </div>
     </>
   );
--- src/pages/newTask.js
+++ src/pages/newTask.js
@@ -32,18 +32,23 @@ export default function NewTask() {
   return (
     <Layout>
       <div>
-        <h1 className="text-3xl text-center text-green-600 my-5">
+        <button
+          className="text-3xl text-center text-green-600 my-5 mx-32
+                     bg-yellow-200 rounded-lg p-2 shadow-2xl
+                     shadow-red-900"
+        >
           Tarea Nueva
-        </h1>
-        <div className="mt-5 md:mt-0 md:col-span-2">
+        </button>
+
+        <div className="mb-8 md:mt-0 md:col-span-2">
           <form method="POST">
             {/*Tarea*/}
             <div className="my-5 flex rounded-md shadow-sm gap-2">
               <span
                 className="inline-flex items-center px-3 py-1
-                          rounded-md border border-gray-300
-                          bg-red-400 text-white
-                          text-bold text-sm"
+                    rounded-md border border-gray-300
+                    bg-red-400 text-white
+                    text-bold text-sm"
               >
                 Tarea
               </span>
@@ -53,24 +58,24 @@ export default function NewTask() {
                 value={task.title}
                 id="idTarea"
                 placeholder="Ingrese una Tarea"
-                className="
-                          bg-white hover:bg-green-400 focus:bg-red-400
-                          text-black hover:text-white focus:text-white
-                          focus:placeholder-red-600
-                          hover:placeholder-green-600
-                          focus:outline-none
-                          flex-1 block w-full rounded
-                          text-sm px-3 py-1"
+                className="bg-white hover:bg-green-400 focus:bg-red-400
+                    text-black hover:text-white focus:text-white
+                    focus:placeholder-red-600
+                    hover:placeholder-green-600
+                    focus:outline-none
+                    flex-1 block w-full rounded
+                    text-sm px-3 py-1"
                 onChange={onChange}
               />
             </div>
+
             {/*Descripcion*/}
-            <div className="my-5 flex rounded-md shadow-sm gap-2">
+            <div className="mt-5 flex rounded-md shadow-sm gap-2">
               <span
                 className="inline-flex items-center px-3 py-1
-                          rounded-md border border-gray-300
-                          bg-red-400 text-white
-                          text-bold text-sm"
+                    rounded-md border border-gray-300
+                    bg-red-400 text-white
+                    text-bold text-sm"
               >
                 Descripción
               </span>
@@ -80,57 +85,64 @@ export default function NewTask() {
                 value={task.description}
                 id="idDescripcion"
                 placeholder="Ingrese una Descripcion"
-                className="
-                          bg-white hover:bg-green-400 focus:bg-red-400
-                          text-black hover:text-white focus:text-white
-                          focus:placeholder-red-600
-                          hover:placeholder-green-600
-                          focus:outline-none
-                          flex-1 block w-full rounded
-                          text-sm px-3 py-1"
+                className="bg-white hover:bg-green-400 focus:bg-red-400
+                    text-black hover:text-white focus:text-white
+                    focus:placeholder-red-600
+                    hover:placeholder-green-600
+                    focus:outline-none
+                    flex-1 block w-full rounded
+                    text-sm px-3 py-1"
                 onChange={onChange}
               />
             </div>
+
             {/*Status*/}
             <div className="my-5 flex rounded-md shadow-sm gap-2">
               <span
                 className="inline-flex items-center px-3 py-1
-                          rounded-md border border-gray-300
-                          bg-red-400 text-white
-                          text-bold text-sm"
+                    rounded-md border border-gray-300
+                    bg-red-400 text-white
+                    text-bold text-sm"
               >
                 Estado
               </span>
-              <input
-                type="text"
+              <select
                 name="status"
                 value={task.status}
-                id="idStatus"
                 placeholder="Pendiente o Terminado"
-                className="
-                          bg-white hover:bg-green-400 focus:bg-red-400
-                          text-black hover:text-white focus:text-white
-                          focus:placeholder-red-600
-                          hover:placeholder-green-600
-                          focus:outline-none
-                          flex-1 block w-full rounded
-                          text-sm px-3 py-1"
+                className="bg-white hover:bg-green-400 focus:bg-red-400
+                    text-black hover:text-white focus:text-white
+                    focus:placeholder-red-600
+                    hover:placeholder-green-600
+                    focus:outline-none
+                    flex-1 block w-full rounded
+                    text-sm px-3 py-1"
                 onChange={onChange}
-              />
+              >
+                <option value="" disabled className="disabled:text-black">
+                  ********** Seleccione el Estado de su Tarea **********
+                </option>
+                <option value="Pendiente">Pendiente</option>
+                <option value="Activo">Activo</option>
+                <option value="Terminado">Terminado</option>
+              </select>
             </div>
+
             {/*Botones*/}
             <div className="flex justify-evenly">
               {/*Boton Save/Guardar*/}
               <button
-                className="inline-flex justify-center py-2 px-4
-                          border border-transparent shadow-sm rounded
-                          text-sm font-medium-md text-white
-                          bg-indigo-600 hover:bg-indigo-700
-                          focus:outline-none focus:ring-2 focus:ring-offset-2
-                          focus:ring-indigo-500
-                          disabled:opacity-50
-                        {disabled ? cursor - not - allowed : cursor - pointer}"
                 disabled={!task.title || !task.description || !task.status}
+                className="inline-flex justify-center py-2 px-4
+                    border border-transparent shadow-sm rounded
+                    text-sm font-medium-md text-white
+                    bg-indigo-600 hover:bg-indigo-700
+                    focus:outline-none focus:ring-2 focus:ring-offset-2
+                    focus:ring-indigo-500
+                    cursor-alias
+                    disabled:opacity-50
+                    disabled:cursor-not-allowed"
+                type="submit"
                 onClick={onSave}
               >
                 Guardar
@@ -138,13 +150,16 @@ export default function NewTask() {
               {/*Cancelar*/}
               <button
                 className="inline-flex justify-center py-2 px-4
-                          border border-transparent shadow-sm rounded
-                          text-sm font-medium-md text-white
-                          bg-red-600 hover:bg-red-700
-                          focus:outline-none focus:ring-2 focus:ring-offset-2
-                          focus:ring-red-500
-                                    disabled:opacity-50
-                                {disabled ? cursor - not - allowed : cursor - pointer}"
+                    border border-transparent shadow-sm rounded
+                    text-sm font-medium-md text-white
+                    bg-red-600 hover:bg-red-700
+                    focus:outline-none focus:ring-2
+                    focus:ring-offset-2
+                    focus:ring-red-500
+                    cursor-alias
+                    disabled:opacity-50
+                    disabled:cursor-not-allowed"
+                type="submit"
                 disabled={!task.title && !task.description && !task.status}
                 onClick={emptyTask}
               >
@@ -152,8 +167,8 @@ export default function NewTask() {
               </button>
             </div>
           </form>
-          <Tareas />
         </div>
+        <Tareas />
       </div>
     </Layout>
   );
